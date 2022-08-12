7e545e14842ef627a3e9fb40374a2e5cfc7801ba
root <root@SP22JCS1.sdc.bsnl.co.in>
2022-08-12 17:02:18 +0530

    run command updated

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
-from backup_project.bkpjobs import backup
-
-backup.details()
-backup.execute()
+from bkpjobs import backup
+from datetime import datetime
+import sys
+if __name__=='__main__':
+    args = sys.argv[1:]
+    if not args:
+        print("Please pass arguments like  details,execute")
+        exit()
+    print('TIME START :',str(datetime.now()))
+    for op in args:
+        getattr(backup,op)()
+    print('TIME END :',str(datetime.now()))
